af62189a1a645b895edb0299b88a09e35392f378
Marcelo Sebastian <cerealkill05@gmail.com>
2023-05-08 10:54:54 -0500

    se agrego nueva seccion, trabajando

--- src/renderer.js
+++ src/renderer.js
@@ -43,3 +43,10 @@ function stopTimer() {
 
 startBtn.addEventListener('click', startTimer);
 stopBtn.addEventListener('click', stopTimer);
+
+//Date
+var months = ['January','February','March','April','May','June','July',
+'August','September','October','November','December'];       
+var tomorrow = new Date();
+tomorrow.setTime(tomorrow.getTime() + (1000*3600*24));       
+document.getElementById("spanDate").innerHTML = months[tomorrow.getMonth()] + " " + tomorrow.getDate()+ ", " + tomorrow.getFullYear();
